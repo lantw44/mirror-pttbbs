4dc4baf757934be65f0e8d2b697734e8cc76ed1c
victor <victor@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2003-08-21 06:07:02 +0000

    reset my branch
    git-svn-id: http://opensvn.csie.org/pttbbs/branches/victor.bank@1115 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

---
+++ pttbbs/pttpi/cgi/t/board.getBid.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+# $Id$
+use Frontier::Client;
+use Data::Dumper;
+do 'host.pl';
+
+$brdname = $ARGV[0] || 'SYSOP';
+$server = Frontier::Client->new(url => $server_url);
+$result = $server->call('board.getBid', $brdname);
+
+print "board.getBid($brdname) from $server_url:\n";
+print Dumper($result);

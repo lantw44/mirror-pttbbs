28fdb84fa48671ce09fa068c68ce22547374587c
jack <jack@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2004-05-12 18:37:11 +0000

    Replaced syspost.c with i18n version
    git-svn-id: http://opensvn.csie.org/pttbbs/branches/Jaky.i18n@1967 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/syspost.c
+++ mbbsd/syspost.c
@@ -17,8 +17,8 @@ post_msg(char *bname, char *title, char *msg, char *author)
     if (!fp)
 	return -1;
 
-    fprintf(fp, "作者: %s 看板: %s\n標題: %s \n", author, bname, title);
-    fprintf(fp, "時間: %s\n", ctime(&now));
+    fprintf(fp, SHM->i18nstr[cuser.language][2551], author, bname, title);
+    fprintf(fp, SHM->i18nstr[cuser.language][2552], ctime(&now));
 
     /* 文章的內容 */
     fprintf(fp, "%s", msg);
@@ -67,15 +67,13 @@ post_change_perm(int oldperm, int newperm, char *sysopid, char *userid)
     if (!(fp = fopen(genbuf, "w")))
 	return;
 
-    fprintf(fp, "作者: [系統安全局] 看板: Security\n"
-	    "標題: [公安報告] 站長修改權限報告\n"
-	    "時間: %s\n", ctime(&now));
+    fprintf(fp, SHM->i18nstr[cuser.language][2553], ctime(&now));
     for (i = 5; i < NUMPERMS; i++) {
 	if (((oldperm >> i) & 1) != ((newperm >> i) & 1)) {
-	    fprintf(fp, "   站長\033[1;32m%s%s%s%s\033[m的權限\n",
+	    fprintf(fp, SHM->i18nstr[cuser.language][2554],
 		    sysopid,
-	       (((oldperm >> i) & 1) ? "\033[1;33m關閉" : "\033[1;33m開啟"),
-		    userid, str_permid[i]);
+	       (((oldperm >> i) & 1) ? SHM->i18nstr[cuser.language][2555] : SHM->i18nstr[cuser.language][2556]),
+		    userid, SHM->i18nstr[cuser.language][str_permid[i]]);
 	    flag++;
 	}
     }
@@ -83,16 +81,16 @@ post_change_perm(int oldperm, int newperm, char *sysopid, char *userid)
     if (flag) {
 	clrtobot();
 	clear();
-	while (!getdata_str(5, 0, "請輸入理由以示負責：",
-			    reason, sizeof(reason), DOECHO, "看板板主:"));
-	fprintf(fp, "\n   \033[1;37m站長%s修改權限理由是：%s\033[m",
+	while (!getdata_str(5, 0, SHM->i18nstr[cuser.language][2557],
+			    reason, sizeof(reason), DOECHO, SHM->i18nstr[cuser.language][2558]));
+	fprintf(fp, SHM->i18nstr[cuser.language][2559],
 		cuser.userid, reason);
 	fclose(fp);
 
 	snprintf(fhdr.title, sizeof(fhdr.title),
-		 "[公安報告] 站長%s修改%s權限報告",
+		 SHM->i18nstr[cuser.language][2560],
 		 cuser.userid, userid);
-	strlcpy(fhdr.owner, "[系統安全局]", sizeof(fhdr.owner));
+	strlcpy(fhdr.owner, SHM->i18nstr[cuser.language][2561], sizeof(fhdr.owner));
 	append_record("boards/S/Security/.DIR", &fhdr, sizeof(fhdr));
     } else
 	fclose(fp);
@@ -126,16 +124,12 @@ post_violatelaw(char *crime, char *police, char *reason, char *result)
     stampfile(genbuf, &fhdr);
     if (!(fp = fopen(genbuf, "w")))
 	return;
-    fprintf(fp, "作者: [Ptt法院] 看板: ViolateLaw\n"
-	    "標題: [報告] %-20s 違法判決報告\n"
-	    "時間: %s\n"
-	    "\033[1;32m%s\033[m判決：\n     \033[1;32m%s\033[m"
-	    "因\033[1;35m%s\033[m行為，\n違反本站站規，處以\033[1;35m%s\033[m，特此公告",
+    fprintf(fp, SHM->i18nstr[cuser.language][2562],
 	    crime, ctime(&now), police, crime, reason, result);
     fclose(fp);
     snprintf(fhdr.title, sizeof(fhdr.title),
-	     "[報告] %-20s 違法判決報告", crime);
-    strlcpy(fhdr.owner, "[Ptt法院]", sizeof(fhdr.owner));
+	     SHM->i18nstr[cuser.language][2563], crime);
+    strlcpy(fhdr.owner, SHM->i18nstr[cuser.language][2564], sizeof(fhdr.owner));
 
     append_record("boards/V/ViolateLaw/.DIR", &fhdr, sizeof(fhdr));
 
@@ -145,11 +139,11 @@ void
 post_newboard(char *bgroup, char *bname, char *bms)
 {
     char            genbuf[256], title[128];
-    snprintf(title, sizeof(title), "[新板成立] %s", bname);
+    snprintf(title, sizeof(title), SHM->i18nstr[cuser.language][2565], bname);
     snprintf(genbuf, sizeof(genbuf),
-	     "%s 開了一個新板 %s : %s\n\n新任板主為 %s\n\n恭喜*^_^*\n",
+	     SHM->i18nstr[cuser.language][2566],
 	     cuser.userid, bname, bgroup, bms);
-    post_msg("Record", title, genbuf, "[系統]");
+    post_msg("Record", title, genbuf, SHM->i18nstr[cuser.language][2567]);
 }
 
 void
@@ -164,17 +158,15 @@ give_money_post(char *userid, int money)
     stampfile(genbuf, &fhdr);
     if (!(fp = fopen(genbuf, "w")))
 	return;
-    fprintf(fp, "作者: [系統安全局] 看板: Security\n"
-	    "標題: [公安報告] 站長%s使用紅包機報告\n"
-	    "時間: %s\n", cuser.userid, ctime(&now));
+    fprintf(fp, SHM->i18nstr[cuser.language][2568], cuser.userid, ctime(&now));
     clrtobot();
     clear();
-    fprintf(fp, "\n   站長\033[1;32m%s\033[m給\033[1;33m%s %d 元\033[m",
+    fprintf(fp, SHM->i18nstr[cuser.language][2569],
 	    cuser.userid, userid, money);
 
     fclose(fp);
-    snprintf(fhdr.title, sizeof(fhdr.title), "[公安報告] 站長%s使用紅包機報告",
+    snprintf(fhdr.title, sizeof(fhdr.title), SHM->i18nstr[cuser.language][2570],
 	     cuser.userid);
-    strlcpy(fhdr.owner, "[系統安全局]", sizeof(fhdr.owner));
+    strlcpy(fhdr.owner, SHM->i18nstr[cuser.language][2571], sizeof(fhdr.owner));
     append_record("boards/S/Security/.DIR", &fhdr, sizeof(fhdr));
 }
